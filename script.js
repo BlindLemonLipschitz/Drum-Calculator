b95cc9a871ef2511a88d974b1eb93e736c6d2cc2
control <git.capybara@gmail.com>
2020-02-07 07:07:52 -0500

    refactor some

--- script.js
+++ script.js
@@ -1,55 +1,60 @@
-var activeBox = "currentNumber";
+var userInputs = {
+  currentNumber: 1,
+  timeInMinutes: 1,
+  drumsNeeded: 1,
+  hoursFromNow: 1
+}
+var inputArray = Object.getOwnPropertyNames(userInputs);
+var activeBox = inputArray[1];
 var selectedBox = 1;
-var inputArray = ["currentNumber","timeInMinutes","drumsNeeded","hoursFromNow"];
-var inputCount = document.getElementsByClassName("field").length;
+var inputCount = inputArray.length;
 var slider = document.getElementById("myRange");
 
-var drum = {
-  currentNumber:1,
-  timeInMinutes:1,
-  drumsNeeded:1,
-  hoursFromNow:1,
-  timeInHours: function() {
-  var val = this.timeInMinutes / 60
-  if (val >= 1) {
-  return Math.round(val)
-  }
-  return 0
+var runInfo = {
+  timeInHours: function () {
+    var val = userInputs.timeInMinutes / 60
+    if (val >= 1) {
+      return Math.round(val)
+    }
+    return 0
   },
-  timeRemainder: function() {
-  return this.timeInMinutes % 60
+  timeRemainder: function () {
+    return userInputs.timeInMinutes % 60
   },
-  runFinished: function() {
-  var mins = this.timeInMinutes * (this.drumsNeeded - this.currentNumber);
-  var time = moment().add(mins, "minutes");
-  return moment(time).format('LLL')
+  runFinished: function () {
+    var mins = userInputs.timeInMinutes * (userInputs.drumsNeeded - userInputs.currentNumber);
+    var time = moment().add(mins, "minutes");
+    return moment(time).format('LLL')
   },
-  futureNumber: function() {
-  var additionalDrums = (this.hoursFromNow * 60 / this.timeInMinutes);
-  var next = Number(drum.currentNumber) + Number(additionalDrums)
-  var time = moment().add(this.hoursFromNow, "hours");
-  var formatedTime = moment(time).format('LLL')
-  return "We should be on drum " + "<span style=\"color:blue;font-size:35px;\"/>" + Math.round(next) + "</span>" + " around " + formatedTime
+  futureNumber: function () {
+    var additionalDrums = (userInputs.hoursFromNow * 60 / userInputs.timeInMinutes);
+    var next = Number(userInputs.currentNumber) + Number(additionalDrums)
+    var time = moment().add(userInputs.hoursFromNow, "hours");
+    var formatedTime = moment(time).format('LLL')
+    return "We should be on drum " + "<span style=\"color:blue;font-size:35px;\"/>" + Math.round(next) + "</span>" + " around " + formatedTime
   }
 }
-
-slider.oninput = function() {
+  function updateMessages() {
+  document.getElementById("message").innerHTML = "Finished around " + runInfo.runFinished();
+  document.getElementById("messageFuture").innerHTML = runInfo.futureNumber();
+}
+slider.oninput = function () {
   document.getElementById(activeBox).value = this.value;
-  drum[activeBox] = this.value;
-  document.getElementById("message").innerHTML = "Finished around " + drum.runFinished();
-  document.getElementById("messageFuture").innerHTML = drum.futureNumber();
+  userInputs[activeBox] = this.value;
+  updateMessages();
 }
-slider.onchange = function() {
+slider.onchange = function () {
   document.getElementById(activeBox).select();
 }
 
 function selectTextDown() {
   var selectedBox = inputArray.indexOf(activeBox);
   if (selectedBox > 0) {
-  selectedBox--;}
-    else {
-      selectedBox = inputCount - 1;
-    }
+    selectedBox--;
+  }
+  else {
+    selectedBox = inputCount - 1;
+  }
   console.log(selectedBox + "selectedBox")
   const input = document.getElementById(inputArray[selectedBox]);
   input.focus();
@@ -58,23 +63,23 @@ function selectTextDown() {
 function selectText() {
   var selectedBox = inputArray.indexOf(activeBox);
   if (selectedBox < (inputCount - 1)) {
-  selectedBox++;}
-    else {
-      selectedBox = 0
-    }
+    selectedBox++;
+  }
+  else {
+    selectedBox = 0
+  }
   const input = document.getElementById(inputArray[selectedBox]);
   input.focus();
   input.select();
-    }
+}
 function updateActiveBox(x) {
   activeBox = x.id;
   document.getElementById("myRange").value = x.value;
 }
-var boxBox = function(x) {
+var boxBox = function (x) {
   document.getElementById(activeBox).value = x.value;
-  drum[activeBox] = x.value;
-  document.getElementById("message").innerHTML = "Finished around " + drum.runFinished();
-  document.getElementById("messageFuture").innerHTML = drum.futureNumber();
+  userInputs[activeBox] = x.value;
+  updateMessages();
 }
 function unHideMessage(x) {
   document.getElementById("message").style.visibility = "visible";
